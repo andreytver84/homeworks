62720fc3b234343e7fe971324f67eb74c539da93
andreytver84 <uvikmedium@mail.ru>
2021-12-11 17:11:22 +0300

    Third task first attempt

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,8 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = false;
+    if (arr1.length === arr2.length && arr1.every((n,i) => n === arr2[i])) {
+      result = true;
+    }
   // Ваш код
 
   return result; // boolean
@@ -8,7 +10,8 @@ function compareArrays(arr1, arr2) {
 
 function advancedFilter(arr) {
   let resultArr;
-
+  const newArr = arr.filter((number) => number > 0 && number % 3 === 0); 
+  resultArr = newArr.map((number) => number*10);
   // Ваш код
 
   return resultArr; // array
