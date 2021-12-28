d2b9fc700fa7cb6cbd127a41b010a9459f755324
andreytver84 <uvikmedium@mail.ru>
2021-12-28 12:22:36 +0300

    second request for help to fifth task

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -8,12 +8,17 @@ class PrintEditionItem {
     }
     fix() {
         this._state = this._state*1.5;
+        if (this._state > 100) {
+            this._state = 100;
+        }
     }
     set state(condition) {
         if (condition < 0) {
             this._state = 0;
         } else if (condition > 100) {
             this._state = 100;
+        } else {
+            this._state = condition;
         }
     }
     get state() {
@@ -22,42 +27,44 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-    constructor(type) {
-       super(type);
+    constructor(name,releaseDate,pagesCount) {
+       super(name,releaseDate,pagesCount);
+       this.type = 'magazine';       
     }
 }
 
 class Book extends PrintEditionItem {
-    constructor(type,author) {
-        super(type);
+    constructor(author,name,releaseDate,pagesCount) {
+       super(name,releaseDate,pagesCount);
+       this.type = 'book';  
        this.author = author;
     }
 }
 
 class NovelBook extends Book {
-    constructor(type,author) {
-        super(type);
-        super(author);
+    constructor(author,name,releaseDate,pagesCount) {   
+        super(author,name,releaseDate,pagesCount);     
+        this.type = 'novel';
      }
 }
 
 class FantasticBook extends Book {
-    constructor(type,author) {
-       super(type);
-       super(author);
+    constructor(author,name,releaseDate,pagesCount) {   
+       super(author,name,releaseDate,pagesCount);    
+       this.type = 'fantastic';
     }
 }
 
 class DetectiveBook extends Book {
-    constructor(type,author) {
-        super(type);
-        super(author);
+    constructor(author,name,releaseDate,pagesCount) {    
+        super(author,name,releaseDate,pagesCount);    
+        this.type = 'detective';
     }
 }
 
 class Library {
-    constructor(namelib) {
-        this.name = namelib;
+    constructor(name) {
+        this.name = name;
         this.books = [];
     }
     addBook(book) {
@@ -74,5 +81,34 @@ class Library {
         } else {
             return null;
         }
+    }
+}
+
+class Student {
+    constructor(name, gender, age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.subjects=[];
+    }
+    addMark(mark,subjectName) {
+        let toSubjects = {subject: subjectName, marks: [mark]};
+        if (this.subjects.find(item => item === subjectName)) {
+           let needSubject = (this.subjects.find(item => item === subjectName));
+           needSubject.marks.push(mark);
+        } else {
+            this.subjects.push(toSubjects);
+        }
+    }
+    getAverageBySubject(subjectName) {
+        let findSubject = this.subjects.find(item => item === subjectName);
+        const sum = findSubject.marks.reduce((acc, mark) => acc + mark, 0);
+        const length = findSubject.marks.length;
+        return sum / length;
+    }
+    getAverage() {
+        const sum = this.subjects.forEach(item.marks.reduce((acc, mark) => acc + mark, 0));
+        const length = this.subjects.length;
+        return sum / length;
     }
 }
